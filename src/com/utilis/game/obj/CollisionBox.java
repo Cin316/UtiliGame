d7cc049259c165c9ab37eb82d6e5f574a3e0d8ae
Cin316 <joe13reich@gmail.com>
2012-07-21 15:47:41 -0400

    Added javadoc comments to CollisionBox.

--- src/com/utilis/game/obj/CollisionBox.java
+++ src/com/utilis/game/obj/CollisionBox.java
@@ -2,11 +2,32 @@ package com.utilis.game.obj;
 
 import java.awt.image.*;
 
+/**
+ * A class for storing the actual solid points that collide with another <code>Collider</code>.
+ * @author Cin316
+ * @see Collider
+ * @see CollisionGroup
+ */
 public class CollisionBox {
 	
-	private int sizeX, sizeY;
-	private boolean[][] collide;
+	/**
+	 * width of the <code>CollisionBox</code>, measured in pixels
+	 */
+	protected int sizeX;
+	/**
+	 * height of the <code>CollisionBox</code>, measured in pixels
+	 */
+	protected int sizeY;
+	/**
+	 * two-dimensional array representing which pixels are solid points that collide with other points.  Its size is equivalent to <code>sizeX</code> and <code>sizeY</code>, respectively 
+	 */
+	protected boolean[][] collide;
 	
+	/**
+	 * class constructor to create blank <code>collide<code> with specified width and height
+	 * @param X width of <code>CollisionBox</code> to be created
+	 * @param Y height of <code>CollisionBox</code> to be created
+	 */
 	public CollisionBox(int X, int Y){
 		
 		collide = new boolean[X][Y];
@@ -18,6 +39,10 @@ public class CollisionBox {
 		}
 		
 	}
+	/**
+	 * class constructor to create <code>collide</code> from an <code>Image</code> (<code>img</code>)  White (RGB[1,1,1]) (or any other color) is considered <code>false</code>.  Black (RGB[255,255,255]) is considered <code>true</code>.  Width and height are specified by width and height of <code>img</code>.
+	 * @param img <code>Image</code> to get <code>collide</code> from
+	 */
 	public CollisionBox(BufferedImage img){
 		
 		sizeX = img.getWidth();
@@ -32,22 +57,46 @@ public class CollisionBox {
 			}
 		}
 	}
+	/**
+	 * class constructor to set <code>collide</code> to provided <code>boolean[][]</code>
+	 * @param bool two-dimensional array to set <code>collide</code> to
+	 */
 	public CollisionBox(boolean[][] bool){
 		
 		collide = bool;
 		
 	}
 	
+	/**
+	 * gets the boolean in <code>collide</code> of provided x and y values
+	 * @param x x position of provided <code>boolean</code> 
+	 * @param y y position of provided <code>boolean</code>
+	 * @return boolean in <code>collide</code> of provided x and y values
+	 */
 	public boolean getCollision(int x, int y){
 		return collide [x][y];
 	}
+	/**
+	 * gets the width of this <code>CollisionBox</code>
+	 * @return the current width of this <code>CollisionBox</code>
+	 */
 	public int getSizeX(){
 		return sizeX;
 	}
+	/**
+	 * gets the width of this <code>CollisionBox</code>
+	 * @return the current width of this <code>CollisionBox</code>
+	 */
 	public int getSizeY(){
 		return sizeY;
 	}
-
+	
+	/**
+	 * sets the boolean specified with <code>x</code> and <code>y</code> to the specified value
+	 * @param x x position of the boolean to be set
+	 * @param y y position of the boolean to be set
+	 * @param value the value to set the specified boolean to
+	 */
 	public void setCollision(int x, int y, boolean value){
 		collide [x][y] = value;
 	}
