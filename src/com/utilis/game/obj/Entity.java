3d5b5fea5afa95b28410a3155a203e5f741d531b
Cin316 <joe13reich@gmail.com>
2012-07-21 18:17:24 -0400

    Added javadoc comments to Entity .

--- src/com/utilis/game/obj/Entity.java
+++ src/com/utilis/game/obj/Entity.java
@@ -5,10 +5,24 @@ import java.awt.MediaTracker;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
+/**
+ * Class representing an object that is not locked to a grid and can collide with other <code>Colliders</code>
+ * @author Cin316
+ * @see Collider
+ * @see com.utilis.game.gui.Canvas
+ */
 public class Entity extends Collider{
 	
+	/**
+	 * the current <code>Image</code> to be displayed
+	 */
 	protected Image image;
 	
+	/**
+	 * class constructor which creates an <code>Entity</code> with the specified width and height, a blank <code>Image</code>, and a blank <code>CollisionBox</code> 
+ 	 * @param w width of the <code>Entity</code> to be created
+	 * @param h height of the <code>Entity</code> to be created
+	 */
 	public Entity(int w, int h){
 		width = w;
 		height = h;
@@ -18,6 +32,10 @@ public class Entity extends Collider{
 		image = (Image) new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		collide = new CollisionBox(width, height);
 	}
+	/**
+	 * class constructor which creates an <code>Entity</code> with the width and height of the specified <code>Image</code>, an <code>Image</code> equivalent to the specified <code>Image</code>, and a blank <code>CollisionBox</code>
+	 * @param i <code>Image</code> to be used for creation of this <code>Entity</code> 
+	 */
 	public Entity(Image i){
 		
 		image = i;
@@ -37,6 +55,10 @@ public class Entity extends Collider{
 		collide = new CollisionBox(width, height);
 		
 	}
+	/**
+	 * class constructor which creates an <code>Entity</code> with the width and height of the specified <code>CollisionBox</code>, a blank <code>Image</code>, and a <code>CollsionBox</code> equivalent to the specified <code>CollisionBox</code>
+	 * @param c <code>CollisionBox</code> to be used for creation of this <code>Entity</code>
+	 */
 	public Entity(CollisionBox c){
 		collide = c;
 		x = 0;
@@ -46,6 +68,11 @@ public class Entity extends Collider{
 		rect = new Rectangle(x, y, width, height);
 		image = (Image) new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 	}
+	/**
+	 * class constructor which creates an <code>Entity</code> with the width and height of the specified <code>CollisionBox</code>, an <code>Image</code> equivalent to the specified <code>Image</code>, and a <code>CollsionBox</code> equivalent to the specified <code>CollisionBox</code>
+	 * @param i <code>Image</code> to be used for creation of this <code>Entity</code>
+	 * @param c <code>CollisionBox</code> to be used for creation of this <code>Entity</code>
+	 */
 	public Entity(Image i, CollisionBox c){
 		collide = c;
 		x = 0;
@@ -56,18 +83,38 @@ public class Entity extends Collider{
 		image = i;
 	}
 	
+	/**
+	 * gets the current <code>Image</code>
+	 * @return the current <code>Image</code> of this <code>Entity</code>
+	 */
 	public Image getImage() {
 		return image;
 	}
 	
+	/**
+	 * sets the current <code>Image</code> to the specified <code>Image</code>
+	 * @param image the <code>Image</code> to set the current <code>Image</code> to
+	 */
 	public void setImage(Image image) {
 		this.image = image;
 	}
 	
+	/**
+	 * sets the position of the <code>Entity</code> to the specified coordinates
+	 * @param x the x coordinate to set the <code>Entity</code> to, measured in pixels
+	 * @param y the y coordinate to set the <code>Entity</code> to, measured in pixels
+	 */
 	public void setPos(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
+	/**
+	 * sets the position of the <code>Entity</code> to the specified coordinates, and sets the <code>width</code> and <code>height</code> to the specified values
+	 * @param x the x coordinate to set the <code>Entity</code> to, measured in pixels
+	 * @param y the y coordinate to set the <code>Entity</code> to, measured in pixels
+	 * @param width the width to set the <code>Entity</code> to, measured in pixels
+	 * @param height the height to set the <code>Entity</code> to, measured in pixels
+	 */
 	public void setBounds(int x, int y, int width, int height){
 		this.x = x;
 		this.y = y;
@@ -75,15 +122,31 @@ public class Entity extends Collider{
 		this.height = height;
 	}
 	
+	/**
+	 * decreases the <code>y</code> position of this <code>Entity</code> the specified number of pixels
+	 * @param number of pixels to move up
+	 */
 	public void moveUp(int n){
 		y -= n;
 	}
+	/**
+	 * increases the <code>y</code> position of this <code>Entity</code> the specified number of pixels
+	 * @param number of pixels to move down
+	 */
 	public void moveDown(int n){
 		y += n;
 	}
+	/**
+	 * decreases the <code>x</code> position of this <code>Entity</code> the specified number of pixels
+	 * @param number of pixels to move left
+	 */
 	public void moveLeft(int n){
 		x -= n;
 	}
+	/**
+	 * increases the <code>x</code> position of this <code>Entity</code> the specified number of pixels
+	 * @param number of pixels to move right
+	 */
 	public void moveRight(int n){
 		x += n;
 	}
