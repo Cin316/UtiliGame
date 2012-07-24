952f1e9cf54d84ed4d0fc63a0e8bad829ab02609
Cin316 <joe13reich@gmail.com>
2012-07-24 11:39:37 -0400

    Added javadoc comments to Window.

--- src/com/utilis/game/gui/Window.java
+++ src/com/utilis/game/gui/Window.java
@@ -1,20 +1,47 @@
 package com.utilis.game.gui;
 
-
 import javax.swing.*;
 
 import com.utilis.game.obj.*;
 
 import java.awt.*;
 
+/**
+ * A class for managing a Canvas.
+ * @author Cin316
+ * @see Canvas
+ */
 public class Window extends JFrame {
 	
+	/**
+	 * the number of pixels wide the <code>Tiles</code> in this <code>Window</code> are
+	 */
+	protected int tileWidth;
+	/**
+	 * the number of pixels high the <code>Tiles</code> in this <code>Window</code> are
+	 */
+	protected int tileHeight;
+	/**
+	 * the number of <code>Tiles</code> wide this <code>Window</code> is
+	 */
+	protected int tilesNumX;
+	/**
+	 * the number of <code>Tiles</code> high this <code>Window</code> is
+	 */
+	protected int tilesNumY;
 	
-	protected int tileWidth, tileHeight;
-	protected int tilesNumX, tilesNumY;
-	
+	/**
+	 * the <code>Canvas</code> in this <code>Window</code>
+	 */
 	protected Canvas canvas;
 	
+	/**
+	 * Class constructor.
+	 * @param xTiles the number of <code>Tiles</code> wide this <code>Window</code> is
+	 * @param yTiles the number of <code>Tiles</code> high this <code>Window</code> is
+	 * @param tWidth the width of the <code>Tiles</code> in this <code>Window</code>
+	 * @param tHeight the height of the <code>Tiles</code> in this <code>Window</code>
+	 */
 	public Window(int xTiles, int yTiles, int tWidth, int tHeight){
 		
 		tilesNumX = xTiles;
@@ -30,32 +57,68 @@ public class Window extends JFrame {
 		this.setVisible(true);
 	}
 	
+	/**
+	 * gets the <code>Canvas</code> in this <code>Window</code>
+	 * @return the current value of the <code>Canvas</code>
+	 */
 	public Canvas getCanvas(){
 		return canvas;
 	}
-
+	
+	/**
+	 * gets the width of the <code>Tiles</code>, measured in pixels
+	 * @return the current width of the <code>Tiles</code>
+	 */
 	public int getTileWidth() {
 		return tileWidth;
 	}
+	/**
+	 * gets the height of the <code>Tiles</code>, measured in pixels
+	 * @return the current height of the <code>Tiles</code>
+	 */
 	public int getTileHeight() {
 		return tileHeight;
 	}
+	/**
+	 * gets the number of <code>Tiles</code> wide this <code>Window</code> is
+	 * @return the current number of <code>Tiles</code> wide this <code>Window</code> is
+	 */
 	public int getTilesNumX() {
 		return tilesNumX;
 	}
+	/**
+	 * gets the number of <code>Tiles</code> high this <code>Window</code> is
+	 * @return the current number of <code>Tiles</code> high this <code>Window</code> is
+	 */
 	public int getTilesNumY() {
 		return tilesNumY;
 	}
 	
+	/**
+	 * sets the width of the <code>Tiles</code> to the specified value
+	 * @param tileWidth the value to set the width of the <code>Tiles</code> to
+	 */
 	public void setTileWidth(int tileWidth) {
 		this.tileWidth = tileWidth;
 	}
+	/**
+	 * sets the height of the <code>Tiles</code> to the specified value
+	 * @param tileHeight the value to set the height of the <code>Tiles</code> to
+	 */
 	public void setTileHeight(int tileHeight) {
 		this.tileHeight = tileHeight;
 	}
+	/**
+	 * sets the number of <code>Tiles</code> wide this <code>Window</code> is to the specified value
+	 * @param tilesNumX the value to set the the number of <code>Tiles</code> wide this <code>Window</code> is to
+	 */
 	public void setTilesNumX(int tilesNumX) {
 		this.tilesNumX = tilesNumX;
 	}
+	/**
+	 * sets the number of <code>Tiles</code> high this <code>Window</code> is to the specified value
+	 * @param tilesNumY the value to set the the number of <code>Tiles</code> high this <code>Window</code> is to
+	 */
 	public void setTilesNumY(int tilesNumY) {
 		this.tilesNumY = tilesNumY;
 	}
