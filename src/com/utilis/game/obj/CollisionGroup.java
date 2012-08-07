93b9ba4fd3ff13399c4c7a07715aa4e0de5830c0
Cin316 <joe13reich@gmail.com>
2012-08-06 22:26:56 -0400

    Added code to check if there is 1 or no Colliders and if so, skip collision checking.

--- src/com/utilis/game/obj/CollisionGroup.java
+++ src/com/utilis/game/obj/CollisionGroup.java
@@ -116,44 +116,45 @@ public class CollisionGroup {
 		boolean collision = false;
 		ArrayList<Collider> collided = new ArrayList<Collider>();
 		
-		// TODO Add code if there is only 1 collider to check.
-		for(int c1=0; c1 <= colliders.size(); c1++){
-			for(int c2=0; c2 <= colliders.size(); c2++){
-				
-				collider1 = colliders.get(c1);
-				collider2 = colliders.get(c2);
-				
-				if(c1==c2 || ((collider1 instanceof Tile)&&(collider2 instanceof Tile)) ){
-					//Skips collision checking.
-					collision = false;
-				}else{
-					//Checks collision.
-					boolean intersects = false;
-					if(collider1.getRectangle().intersects(collider2.getRectangle())){
-						intersects = true;
-						intersection = collider1.getRectangle().intersection(collider2.getRectangle());
-						offset1X = intersection.x - collider1.getX();
-						offset2X = intersection.x - collider2.getX();
-						offset1Y = intersection.y - collider1.getY();
-						offset2Y = intersection.y - collider2.getY();
-						
-						for(int x=0; x <= intersection.getWidth(); x++){
-							for(int y=0; y <= intersection.getHeight(); y++){
-								collision = collider1.getCollisionBox().getCollision(collider1.getX()+offset1X+x, collider1.getY()+offset1Y+y) && collider2.getCollisionBox().getCollision(collider2.getX()+offset2X+x, collider2.getY()+offset2Y+y) || collision;
-								
-							}
-						}
-					}else{
+		if( !(collided.size() == 1 || collided.size() == 0) ){
+			for(int c1=0; c1 <= colliders.size(); c1++){
+				for(int c2=0; c2 <= colliders.size(); c2++){
+					
+					collider1 = colliders.get(c1);
+					collider2 = colliders.get(c2);
+					
+					if(c1==c2 || ((collider1 instanceof Tile)&&(collider2 instanceof Tile)) ){
+						//Skips collision checking.
 						collision = false;
+					}else{
+						//Checks collision.
+						boolean intersects = false;
+						if(collider1.getRectangle().intersects(collider2.getRectangle())){
+							intersects = true;
+							intersection = collider1.getRectangle().intersection(collider2.getRectangle());
+							offset1X = intersection.x - collider1.getX();
+							offset2X = intersection.x - collider2.getX();
+							offset1Y = intersection.y - collider1.getY();
+							offset2Y = intersection.y - collider2.getY();
+							
+							for(int x=0; x <= intersection.getWidth(); x++){
+								for(int y=0; y <= intersection.getHeight(); y++){
+									collision = collider1.getCollisionBox().getCollision(collider1.getX()+offset1X+x, collider1.getY()+offset1Y+y) && collider2.getCollisionBox().getCollision(collider2.getX()+offset2X+x, collider2.getY()+offset2Y+y) || collision;
+									
+								}
+							}
+						}else{
+							collision = false;
+						}
+						if (collision){
+							collided.add(collider1);
+							collided.add(collider2);
+						}
 					}
-					if (collision){
-						collided.add(collider1);
-						collided.add(collider2);
-					}
+						
 				}
 					
 			}
-				
 		}
 		return new Collision(collided, collision);
 	}
